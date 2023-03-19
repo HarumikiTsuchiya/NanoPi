bc6a8b705fbfb69165615fc458b7bd0dd1129972
Harumiki Tsuchiya <harumiki.tsuchiya@gmail.com>
2023-03-19 14:03:00 +0900

    リモートでタイマー時間を取得

--- power_on.py
+++ power_on.py
@@ -2,7 +2,7 @@
 
 import nanopi_timer
 import os
-
+import datetime
 
 def get_sw():
 
@@ -15,8 +15,20 @@ def get_sw():
             output = stream.read()
 
             if "0" in output:
+                nanopi_alarm=nanopi_timer.alarm_set()
+
+                now=datetime.datetime.now()
+                print(now)
+
+                alarm_hour,alarm_minute = nanopi_alarm.next_alarm_time(now.hour,now.minute)
+
+                nanopi_alarm.alarm_set(alarm_hour,alarm_minute)
+                nanopi_alarm.alarm_start()
+
                 nanopi = nanopi_timer.Nanopi_control()
                 nanopi.shutdown()
+                    run_command()
+
 
     except KeyboardInterrupt:
         print("interrupted!")
