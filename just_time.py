20006b610027bef164ebf0a51679b773269c363e
Harumiki Tsuchiya <harumiki.tsuchiya@gmail.com>
2023-03-19 14:39:01 +0900

    host名を.envに格納

--- just_time.py
+++ just_time.py
@@ -6,18 +6,38 @@ import os
 import recv_DM
 import datetime
 import requests
+import send_command
+from dotenv import load_dotenv
+
 
 def run_command():
     print("Just Time")
+    # 環境変数読込
+    load_dotenv()
+    env_host_url = os.getenv("host_url")
+    env_local_folder = os.getenv("local_folder")
+
+    log_text = ""
 
     nanopi = nanopi_timer.Nanopi_control()
     nanopi.led_output("Y", 1)
 
-    data,log_text=recv_DM.recv_dm()
+    print("DM get")
+    data, log_text = recv_DM.recv_dm()
+
+    ret = send_command.download_command_list()
+    print(ret)
+    if ret == "OK":
+        ret, logtext = send_command.send_command_list()
+        print(ret)
+        print(logtext)
+        log_text += "Send Command List\n"
+        log_text += logtext
 
     print("Alarm Table Download")
-    url = "https://tmlnagoya.meter.jp/test_power/Alarm_Time.json"
-    filename = "/home/tsuchiya/NanoPi/Alarm_Time.json"
+
+    url = "https://" + env_host_url + "/Alarm_Time.json"
+    filename = env_local_folder + "/Alarm_Time.json"
 
     urlData = requests.get(url)
     print("URL:" + url)
@@ -28,22 +48,28 @@ def run_command():
         with open(filename, mode="wb") as f:
             f.write(urlData.content)
 
+    print("ADC DC Volt")
     adc = nanopi_timer.MCP3425()
     adc.init()
     dc_volt = adc.read_data()
     print("DC Volt : {} V".format(dc_volt))
 
-    nanopi_alarm=nanopi_timer.alarm_set()
+    nanopi_alarm = nanopi_timer.alarm_set()
 
-    now=datetime.datetime.now()
+    now = datetime.datetime.now()
     print(now)
 
-    alarm_hour,alarm_minute = nanopi_alarm.next_alarm_time(now.hour,now.minute)
-
+    alarm_hour, alarm_minute = nanopi_alarm.next_alarm_time(now.hour, now.minute)
 
     ret = sendmail.sendmail(
         mail_to="harumiki.tsuchiya@gmail.com",
-        mail_data="Nano Pi Wake Up!!\nDC Volt : {} V\nData recv: ".format(dc_volt) + data + "\n------\n" + log_text + "\n------\nAlarm Table Recv:{}\nNext WakeUp Time : {}:{} \n".format(urlData.status_code, alarm_hour,alarm_minute),
+        mail_data="Nano Pi Wake Up!!\nDC Volt : {} V\nData recv: ".format(dc_volt)
+        + data
+        + "\n------\n"
+        + log_text
+        + "\n------\nAlarm Table Recv:{}\nNext WakeUp Time : {}:{} \n".format(
+            urlData.status_code, alarm_hour, alarm_minute
+        ),
         subject="Nano Pi Wake up",
     )
     if ret == True:
@@ -53,23 +79,24 @@ def run_command():
         nanopi.led_output("Y", 0)
         nanopi.led_output("R", 1)
 
-    rtc=nanopi_timer.RX8900A()
+    rtc = nanopi_timer.RX8900A()
     rtc.init()
-    ret=rtc.write_ntp_time()
+    ret = rtc.write_ntp_time()
     if ret == "NG":
-        ret=rtc.write_rtc_time()
-    ret=rtc.read_time()
+        ret = rtc.write_rtc_time()
+    ret = rtc.read_time()
 
-    #nanopi_alarm=nanopi_timer.alarm_set()
+    # nanopi_alarm=nanopi_timer.alarm_set()
 
-    now=datetime.datetime.now()
+    now = datetime.datetime.now()
     print(now)
 
-    alarm_hour,alarm_minute = nanopi_alarm.next_alarm_time(now.hour,now.minute)
-    nanopi_alarm.alarm_set(alarm_hour,alarm_minute)
+    alarm_hour, alarm_minute = nanopi_alarm.next_alarm_time(now.hour, now.minute)
+    nanopi_alarm.alarm_set(alarm_hour, alarm_minute)
     nanopi_alarm.alarm_start()
 
     nanopi.shutdown()
+
 
 if __name__ == "__main__":
     run_command()
--- send_command.py
+++ send_command.py
@@ -1,30 +1,125 @@
-import nanopi_timer
-import soracom
-import json
-import time
+import serial
+import datetime
+from ftplib import FTP
+from dotenv import load_dotenv
+import os
 
 
-log_txt=""
+def download_command_list():
+    # 環境変数読込
+    load_dotenv()
+    env_ftp_server = os.getenv("ftp_server")
+    env_ftp_user = os.getenv("ftp_user")
+    env_ftp_passwd = os.getenv("ftp_passwd")
+    env_ftp_folder = os.getenv("ftp_folder")
 
-with open('command_list.txt', 'r') as file:
-    command_list = file.readlines()
+    print("Download Command List")
 
-nanopi = nanopi_timer.serial_control()
+    try:
+        ftp = FTP(env_ftp_server, env_ftp_user, env_ftp_passwd, timeout=5)
+        # ftp.encoding = "utf-8"
 
-log_txt += "ESC+ON"
+        filename = "command_list.txt"
 
-ret = nanopi.send_ESCON()
+        with open(filename, "wb") as f:
+            ftp.retrbinary("RETR " + env_ftp_folder + "/" + filename, f.write)
+
+        ftp.rename(
+            env_ftp_folder + "/" + filename, env_ftp_folder + "/" + filename + "_old"
+        )
+
+        return "OK"
+
+    except:
+        print("ftp error")
+        return "NG"
 
 
-for command in command_list:
-    #print(command.strip())
+def upload_command_list():
+    print("Upload Command List")
 
-    log_txt += command
 
-    ret = nanopi.send_command(command.strip())
+def clr_dm_num_file():
+    print("Delete Dm_num file")
+    filename = "DM_num.txt"
+    with open(filename, mode="w") as f:
+        pass
 
-    log_txt += ret
 
-#log_txt += "ESC+OFF"
-#ret = nanopi.send_ESCOF()
+def send_command_list():
+    filename = "command_list.txt"
+    with open(filename, mode="r", encoding="utf-8") as f:
+        file_list = f.read()
+        command_list = file_list.split("\n")
+        command_list = list(filter(None, command_list))
+        print(command_list)
 
+    log_text = ""
+    ret = "OK"
+
+    for command in command_list:
+        if command[0] != "#":
+            now = datetime.datetime.now()
+            now_str = now.strftime("%y/%m/%d %H:%M:%S")
+            # print(command)
+            log_text += "[" + now_str + "]\n"
+            log_text += command + "\n"
+            if "{Datetime}" in command:
+                command = command.replace("{Datetime}", now_str)
+                log_text += command + "\n"
+            if "{CLR_DM_num}" in command:
+                clr_dm_num_file()
+                continue
+            ret, data = send_command(command)
+            # ret="OK"
+            log_text += data
+            if ret == "NG":
+                break
+
+    return ret, log_text
+
+
+def send_command(command):
+    ser = serial.Serial("COM15", 9600, timeout=5)
+    ser.close()
+    ser.open()
+
+    buf = ""
+    data = ""
+    command = command + "\r\n"
+    ser.write(command.encode("utf-8"))
+
+    while 1:
+
+        # print(frm)
+        buf = ser.readline()
+        try:
+            buf.decode()
+        #            print(buf)
+        except:
+            # TC-35Nのバッファエラー対策(スリープ時に制御文字が入る))
+            data = data + buf[1:].decode()
+        else:
+            data = data + buf.decode()
+
+        if "END" in data:
+            print(data)
+            ser.reset_input_buffer()
+            rcv_OK = True
+            return "OK", data
+        if "ERR" in data:
+            print(data)
+            ser.reset_input_buffer()
+            rcv_OK = True
+            return "NG", data
+
+
+if __name__ == "__main__":
+    ret = download_command_list()
+    print(ret)
+    if ret == "OK":
+        ret, logtext = send_command_list()
+        print(ret)
+        print(logtext)
+
+        # send_command("RT23/1/1 1:2:3")
